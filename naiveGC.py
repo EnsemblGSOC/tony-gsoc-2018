f857d4068e7acabdfb486f9d6c6d27bc61c7b5a1
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-05-23 13:33:07 +0100

    Implement shift window support

--- naiveGC.py
+++ naiveGC.py
@@ -15,13 +15,12 @@ args = parser.parse_args()
 def percentage(byte):
     if byte in ['G', 'C']:
         return percentage_bp
-    else:
-        return 0
+    return 0
 
 
 def generate_wiggle(input_file, output_file, window_size, shift):
     """"""
-    basepair_location = 0
+    basepair_location = 1
     counter = 0
     total_percent = 0
     global percentage_bp
@@ -32,15 +31,15 @@ def generate_wiggle(input_file, output_file, window_size, shift):
     else:
         result = open(input_file.split(".")[0] + ".wig", "w+")
 
-    with open(input_file) as f:
+    with open(input_file, "rb") as f:
         # read track line
-        title = f.readline()
+        title = f.readline().decode('utf-8')
         result.write(title)
         # add step info
         result.write("variableStep span=" + str(window_size) + "\n")
         while True:
             # read file one bp at a time
-            a = f.read(1)
+            a = f.read(1).decode('utf-8')
             # if not EOF and not newline
             if a != "" and a != "\n":
                 counter += 1
@@ -48,18 +47,18 @@ def generate_wiggle(input_file, output_file, window_size, shift):
                 # if reached window size, write percentage
                 if counter == window_size:
                     result.write(str(basepair_location) + "  " + str(int(total_percent)) + "\n")
-                    basepair_location += window_size
+                    basepair_location = basepair_location + shift
+                    f.seek(-(window_size - shift), 1)
                     counter = 0
                     total_percent = 0
             elif a == '':
                 # if end of file and still bp remains
                 if counter != 0:
                     result.write(str(basepair_location) + "  " + str(int(total_percent * window_size / counter)) + "\n")
-                    basepair_location += window_size
+                    basepair_location += shift
                     break
                 else:
                     break
-
     result.close()
 
 
