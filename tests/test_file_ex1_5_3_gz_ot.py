b1597fc7c493cba4298be7de27e81f91b3b64e13
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-08-24 11:46:37 +0800

    Add copyright info

--- tests/test_file_ex1_5_3_gz_ot.py
+++ tests/test_file_ex1_5_3_gz_ot.py
@@ -1,4 +1,18 @@
 """
+.. See the NOTICE file distributed with this work for additional information
+   regarding copyright ownership.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+"""
 Test_1
 """
 
@@ -9,7 +23,7 @@ import subprocess
 def test_1():
     """Test_1"""
     subprocess.run(["cwl-runner",
-					r"--outdir=./tests",
+                    r"--outdir=./tests",
                     "./workflow/Retrieve_GC_workflow.cwl",
                     "./tests/test_file_ex1_5_3_gz_ot.yml"])
     subprocess.run(["gzip", "-d", "-f", "./tests/ex1_5_3_gz_ot_test.wig.gz"])
